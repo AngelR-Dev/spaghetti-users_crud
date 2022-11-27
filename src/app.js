eb5511cd1de5aa61c7b0b3b1546dfcfa4e7fe708
AngelR-Dev <111716367+AngelR-Dev@users.noreply.github.com>
2022-11-27 00:00:41 -0600

    fixed code

--- src/app.js
+++ src/app.js
@@ -1,13 +1,41 @@
-const express = require('express')
+const express = require("express");
+const db = require('./utils/database')
+const userRouter = require('./users/users.router')
 
-const port = 9000
+const app = express();
+const port = 9000;
 
-const app = express
+db.authenticate()
+  .then(() => {
+    console.log('Database Authenticated!')
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'Ok!'})
-}) 
+db.sync()
+  .then(() => {
+    console.log('Database Synced!')
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 
-app.listen(() => {
-    console.log(`Server started at port ${port}`)
-})
+app.use(express.json());
+
+app.use('/api/v1', userRouter)
+
+// app.get('/', (req, res) => {
+//     res.status(200).json({message: 'Ok!'})
+// })
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "OK",
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`Server started at port ${port}`);
+});
--- src/utils/database.js
+++ src/utils/database.js
@@ -1,2 +1,12 @@
+const { Sequelize } = require('sequelize')
 
+const db = new Sequelize({
+  dialect: 'postgres',
+  host: 'localhost',
+  database: 'userCrud-spagetti',
+  username: 'postgres',
+  password: 'root',
+  port: 5432
+})
 
+  module.exports = db
